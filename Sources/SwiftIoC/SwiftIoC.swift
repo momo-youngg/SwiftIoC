caba2d1428125489e9a8d701961af85becd874f5
momo <yeongmoj94+dev@gmail.com>
2024-05-28 02:19:56 +0900

    modify component macro names attribute property

--- Sources/SwiftIoC/SwiftIoC.swift
+++ Sources/SwiftIoC/SwiftIoC.swift
@@ -13,5 +13,5 @@ public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "S
 @attached(accessor)
 public macro Autowired() = #externalMacro(module: "SwiftIoCMacros", type: "AutowiredMacro")
 
-@attached(member, names: named(`init`))
+@attached(member, names: named(init))
 public macro Component() = #externalMacro(module: "SwiftIoCMacros", type: "ComponentMacro")
