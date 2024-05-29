f94f07442647149718427bfaa706ec8da10a2010
momo <yeongmoj94+dev@gmail.com>
2024-05-29 23:41:57 +0900

    test if component macro works well with autowired property

--- Tests/SwiftIoCTests/DefaultDIContainerTests.swift
+++ Tests/SwiftIoCTests/DefaultDIContainerTests.swift
@@ -14,6 +14,12 @@ final class DefaultDIContainerTests: XCTestCase {
     public class NoDependencyClass {
         let normalProperty: Int = 1
     }
+    
+    @Component
+    public class SingleDependencyClass {
+        @Autowired
+        var dependency: NoDependencyClass
+    }
 
     func test_DIContainer_returns_proper_instance() {
         let sut: DIContainer = DIContainer.shared
@@ -21,5 +27,14 @@ final class DefaultDIContainerTests: XCTestCase {
         let actual: Componentable = sut.resolve(NoDependencyClass.self)
         
         XCTAssertTrue(actual is NoDependencyClass)
+    }
+    
+    func test_DIContainer_works_having_dependency_class() {
+        let sut: DIContainer = DIContainer.shared
+        
+        let actual: Componentable = sut.resolve(SingleDependencyClass.self)
+        
+        XCTAssert(actual is SingleDependencyClass)
+        XCTAssert((actual as? SingleDependencyClass)?.dependency is NoDependencyClass)
     }
 }
