b90161bd883ab7f0bb0f944c4fe26a4086a069e5
momo <yeongmoj94+dev@gmail.com>
2024-06-01 20:12:24 +0900

    remove useless sample macro

--- Sources/SwiftIoC/SwiftIoC.swift
+++ Sources/SwiftIoC/SwiftIoC.swift
@@ -1,15 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-/// A macro that produces both a value and a string containing the
-/// source code that generated the value. For example,
-///
-///     #stringify(x + y)
-///
-/// produces a tuple `(x + y, "x + y")`.
-@freestanding(expression)
-public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "SwiftIoCMacros", type: "StringifyMacro")
-
 @attached(accessor)
 public macro Autowired(container: DependencyResolvable = DIContainer.shared) = #externalMacro(module: "SwiftIoCMacros", type: "AutowiredMacro")
 
